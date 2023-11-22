abac296a537fb9ee16c35c01e19b38ccb9f1aa19
Suryank <suryanksingh_2@outlook.com>
2023-11-23 00:50:42 +0530

    commit all

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="glass">
+        This is a glass morphosis design container
+      </div>
+      <img src={logo} className="App-logo" alt="logo" />
+    </>
+  );
+}
+
+export default App;
